f909cc8e600def7d7122679a1572fa931a3fe81c
markusedgar <markus@workplayexperience.com>
2023-07-23 15:49:45 +0200

    dqwqw

--- pages/Concept_sketch.py
+++ pages/Concept_sketch.py
@@ -3,86 +3,84 @@ from langchain import PromptTemplate
 from langchain.llms import OpenAI
  
 template = """
-    Below is an email that may be poorly worded.
-    Your goal is to:
-    - Properly format the email
-    - Convert the input text to a specified tone
-    - Convert the input text to a specified dialect
-
-    Here are some examples different Tones:
-    - Formal: We went to Barcelona for the weekend. We have a lot of things to tell you.
-    - Informal: Went to Barcelona for the weekend. Lots to tell you.  
-
-    Here are some examples of words in different dialects:
-    - American: French Fries, cotton candy, apartment, garbage, cookie, green thumb, parking lot, pants, windshield
-    - British: chips, candyfloss, flag, rubbish, biscuit, green fingers, car park, trousers, windscreen
-
-    Example Sentences from each dialect:
-    - American: I headed straight for the produce section to grab some fresh vegetables, like bell peppers and zucchini. After that, I made my way to the meat department to pick up some chicken breasts.
-    - British: Well, I popped down to the local shop just the other day to pick up a few bits and bobs. As I was perusing the aisles, I noticed that they were fresh out of biscuits, which was a bit of a disappointment, as I do love a good cuppa with a biscuit or two.
-
-    Please start the email with a warm introduction. Add the introduction if you need to.
+    Below is a description of a new service business concept, a target audience (as a persona description), and the scope of the journey.     
     
-    Below is the email, tone, and dialect:
-    TONE: {tone}
-    DIALECT: {dialect}
-    EMAIL: {email}
+    CONCEPT: 
+    {concept}
+
+    PERSONA: 
+    {persona}
+
+    SCOPE: 
+    {scope}
+
+    For the given concept create customer experience as a step-by-step journey map. 
     
-    YOUR {dialect} RESPONSE:
+    Mark all steps of the journey either as
+    * direct touchpoints (iteracting with the consultancy, e.g. meeting, workshop or phone call)
+    * Indirect touchpoints (interacting with information about the consultancy, e.g. review sites, word of mouth)
+    * Internal steps (not interacting with the consultancy, e.g. when making internal decisions, comparing alternatives, finding budgets etc.)
+
+    Create a step-by-step journey map, describing activities and experiences of the given persona at each step. 
+    Use {person_select} for the description.
+
+    Output the journey map as a markdown table with steps going from left to right as columns like the following format
+
+    (step title) | (step title) | (step title) | (step title) | …
+    --- | --- | --- | --- | --- 
+    ( description) | (description) | (description) | (description) | …
+
 """
 
 prompt = PromptTemplate(
-    input_variables=["tone", "dialect", "email"],
+    input_variables=["persona", "concept", "scope", "person_select"],
     template=template,
 )
 
 def load_LLM(openai_api_key):
     """Logic for loading the chain you want to use should go here."""
     # Make sure your openai_api_key is set as an environment variable
-    llm = OpenAI(temperature=.7, openai_api_key=openai_api_key)
+    llm = OpenAI(openai_api_key=openai_api_key)
     return llm
 
-st.set_page_config(page_title="Globalize Email", page_icon=":robot:")
-st.header("Globalize Text")
+st.set_page_config(page_title="Draft an assumption-based future-state journey", page_icon=":robot:")
+st.header("TiSDD Journey Map Generator")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.markdown("Often professionals would like to improve their emails, but don't have the skills to do so. \n\n This tool \
-                will help you improve your email skills by converting your emails into a more professional format. This tool \
-                is powered by [LangChain](https://langchain.com/) and [OpenAI](https://openai.com) and made by \
-                [@GregKamradt](https://twitter.com/GregKamradt). \n\n View Source Code on [Github](https://github.com/gkamradt/globalize-text-streamlit/blob/main/main.py)")
+    st.markdown("Explanation")
 
 with col2:
     st.image(image='TweetScreenshot.png', width=500, caption='https://twitter.com/DannyRichman/status/1598254671591723008')
 
-st.markdown("## Enter Your Email To Convert")
+st.markdown("## Some heading")
 
 def get_api_key():
     input_text = st.text_input(label="OpenAI API Key ",  placeholder="Ex: sk-2twmA8tfCb8un4...", key="openai_api_key_input")
     return input_text
 
-openai_api_key = get_api_key()
+openai_api_key = st.secrets.wpxspecial.OPENAIAPIKEY
 
 col1, col2 = st.columns(2)
 with col1:
-    option_tone = st.selectbox(
-        'Which tone would you like your email to have?',
-        ('Formal', 'Informal'))
+    option_person = st.selectbox(
+        'Which person would you like to use for the journey?',
+        ('first person (me, myself, I)', 'third person (he/she/them)'))
     
 with col2:
-    option_dialect = st.selectbox(
-        'Which English Dialect would you like?',
-        ('American', 'British'))
+    option_scope = st.selectbox(
+        'Which scope would you like to look at?',
+        ('end-2-end', ''))
 
 def get_text():
-    input_text = st.text_area(label="Email Input", label_visibility='collapsed', placeholder="Your Email...", key="email_input")
+    input_text = st.text_area(label="Concept input", label_visibility='collapsed', placeholder="Your concept...", key="concept_input")
     return input_text
 
-email_input = get_text()
+concept_input = get_text()
 
-if len(email_input.split(" ")) > 700:
-    st.write("Please enter a shorter email. The maximum length is 700 words.")
+if len(concept_input.split(" ")) > 500:
+    st.write("Please enter a shorter concept description. The maximum length is 500 words.")
     st.stop()
 
 def update_text_with_example():
@@ -93,15 +91,15 @@ st.button("*See An Example*", type='secondary', help="Click to see an example of
 
 st.markdown("### Your Converted Email:")
 
-if email_input:
+if concept_input:
     if not openai_api_key:
         st.warning('Please insert OpenAI API Key. Instructions [here](https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key)', icon="⚠️")
         st.stop()
 
     llm = load_LLM(openai_api_key=openai_api_key)
 
-    prompt_with_email = prompt.format(tone=option_tone, dialect=option_dialect, email=email_input)
+    prompt_with_concept = prompt.format(persona=option_persona, concept=concept_input, person=option_person, scope=input_scope)
 
-    formatted_email = llm(prompt_with_email)
+    journey_draft = llm(prompt_with_concept)
 
-    st.write(formatted_email)
+    st.write(journey_draft)
